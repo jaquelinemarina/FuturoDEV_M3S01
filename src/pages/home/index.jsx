dcb00ae3590429af99f904951c8c8c0f215d09ce
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-09-02 15:39:31 -0300

    ✨ feat (home): criando homepage

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Temporizador</h1>
+            <p>Bem-vindo à página principal da aplicação!</p>
+        </div>
+    )
+}
+
+export default HomePage
