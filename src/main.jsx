54b530b78f79fc84b5d9d3cfa137ff2167d673b1
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-09-02 20:57:00 -0300

    ⏪ revert: merge "feature/ex03-contexto-autenticação" into "master"

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,7 @@
-import React from "react"
-import App from "./App.jsx"
-import { AuthProvider } from "./contexts/AuthContext.jsx"
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from "react-router-dom"
-import routers from "./router/routes"
-import "./styles/globals.css"
+import App from './App.jsx'
+import './styles/globals.css'
 
 createRoot(document.getElementById('root')).render(
-    <AuthProvider>
-        <RouterProvider router={routers}>
-            <App />
-        </RouterProvider>
-    </AuthProvider>
+    <App />
 )
