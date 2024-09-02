6201cefeae05dc5531fb05b0634979122c8e5101
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-09-02 20:38:06 -0300

    ✨ feat (autenticação): implementando provider de autenticação na main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import App from "./App.jsx"
+import { AuthProvider } from "./contexts/AuthContext.jsx"
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom"
 import routers from "./router/routes"
 import "./styles/globals.css"
 
 createRoot(document.getElementById('root')).render(
-    <RouterProvider router={routers}>
-        <App />
-    </RouterProvider>
+    <AuthProvider>
+        <RouterProvider router={routers}>
+            <App />
+        </RouterProvider>
+    </AuthProvider>
 )
